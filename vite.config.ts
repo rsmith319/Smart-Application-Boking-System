884f6b5b04b5af8728f26375e9d3c1c72651c8ff
Koder <140269220+rsmith319@users.noreply.github.com>
2026-04-05 19:03:54 -0400

    v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//       "@components": path.resolve(__dirname, "src/components"),
+//       "@pages": path.resolve(__dirname, "src/pages"),
+//       "@data": path.resolve(__dirname, "src/data"),
+//       "@services": path.resolve(__dirname, "src/services"),
+//       // "@utils": path.resolve(__dirname, "src/utils")
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
@@ -10,8 +28,7 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "src/components"),
       "@pages": path.resolve(__dirname, "src/pages"),
       "@data": path.resolve(__dirname, "src/data"),
-      "@services": path.resolve(__dirname, "src/services"),
-      // "@utils": path.resolve(__dirname, "src/utils")
-    },
-  },
+      "@services": path.resolve(__dirname, "src/services")
+    }
+  }
 });

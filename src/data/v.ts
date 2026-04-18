ef7c070e58f38dcaef0e404d5ffc56492687fff4
Koder <140269220+rsmith319@users.noreply.github.com>
2026-04-18 18:27:16 -0400

    refactor

--- src/data/v.ts
+++ src/data/v.ts
@@ -1,19 +1,19 @@
-// const BASE_URL: string = "https://api.wittyground-e489ec01.westus2.azurecontainerapps.io/api/v1"; //Laptop IP
-const localHost: string = "https://localhost:3000/"
-const server: string = "https://api.wittyground-e489ec01.westus2.azurecontainerapps.io/api/v1"
+const BASE_URL: string = "https://api.wittyground-e489ec01.westus2.azurecontainerapps.io/api/v1"; //Laptop IP
+// const localHost: string = "https://localhost:3000/"
+// const server: string = "https://api.wittyground-e489ec01.westus2.azurecontainerapps.io/api/v1"
 
 
-async function ApiSwitcher(api: string) {
-  const response = await fetch(api);
-  if (!response.ok) {
-    return false;
-  }
-  return true;
-}
+// async function ApiSwitcher(api: string) {
+//   const response = await fetch(api);
+//   if (!response.ok) {
+//     return false;
+//   }
+//   return true;
+// }
 
-// TODO -> use ApiSwitcher function to switch between dev server and deployed api
+// // TODO -> use ApiSwitcher function to switch between dev server and deployed api
 
-const BASE_URL: string = (await ApiSwitcher("https://localhost:3000/")) ? localHost : server;
+// const BASE_URL: string = (await ApiSwitcher("https://localhost:3000/")) ? localHost : server;
 
 
 // =========================
